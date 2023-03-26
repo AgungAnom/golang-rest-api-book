edf54c297ce3954958f4cc866fbe645473ff8a59
I Gusti Agung Anom Semara Putra <agoenkanom1305@gmail.com>
2023-03-27 02:49:13 +0800

    finish

--- controllers/bookController.go
+++ controllers/bookController.go
@@ -1,7 +1,9 @@
 package controllers
 
 import (
+	"fmt"
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
@@ -26,7 +28,105 @@ func CreateBook(ctx *gin.Context){
 	bookDatas = append(bookDatas,newBook)
 
 
-	ctx.JSON(http.StatusOK,gin.H{
-	"message":"Created",
-	})
+	show := []byte(`"Created"`)
+	ctx.Data(http.StatusOK,"application/json", show)
 }
+
+func GetBook(ctx *gin.Context){
+	bookId := ctx.Param("id")
+	idData,_ := strconv.Atoi(bookId)
+	state := false
+	var bookData Book
+
+
+	for _, book := range bookDatas{
+		if idData == book.BookID{
+			state = true
+			bookData = book
+			break
+		}
+	}
+
+	if !state{
+		ctx.AbortWithStatusJSON(http.StatusNotFound,gin.H{
+			"error_message": fmt.Sprintf("Book with id %v not found", idData),
+			})
+	} else {
+		ctx.JSON(http.StatusOK,bookData)
+	}
+
+	
+}
+
+func UpdateBook(ctx *gin.Context){
+	bookId := ctx.Param("id")
+	idData,_ := strconv.Atoi(bookId)
+	state := false
+	var updatedBook Book
+
+		if err := ctx.ShouldBindJSON(&updatedBook); err != nil{
+				ctx.AbortWithError(http.StatusBadRequest, err)
+				return
+			}
+
+	
+	for i, book := range bookDatas{
+		if idData == book.BookID{
+			state = true
+			bookDatas[i] = updatedBook
+			bookDatas[i].BookID = idData
+			break
+		}
+	}
+
+	if !state{
+		ctx.AbortWithStatusJSON(http.StatusNotFound,gin.H{
+			"error_message": fmt.Sprintf("Book with id %v not found", idData),
+			})
+	} else {
+		show := []byte(`"Updated"`)
+		ctx.Data(http.StatusOK,"application/json", show)
+	}
+	
+}
+
+
+func DeleteBook(ctx *gin.Context){
+	bookId := ctx.Param("id")
+	idData,_ := strconv.Atoi(bookId)
+	state := false
+	var indexBook int
+
+
+	
+	for i, book := range bookDatas{
+		if idData == book.BookID{
+			state = true
+			indexBook = i
+			break
+		}
+	}
+
+	if !state{
+		ctx.AbortWithStatusJSON(http.StatusNotFound,gin.H{
+			"error_message": fmt.Sprintf("Book with id %v not found", idData),
+			})
+		return
+	}
+
+
+
+	if state {
+		copy(bookDatas[indexBook:], bookDatas[indexBook+1:])
+		bookDatas[len(bookDatas)-1] = Book{}
+		bookDatas = bookDatas[:len(bookDatas)-1]
+
+		show := []byte(`"Deleted"`)
+		ctx.Data(http.StatusOK,"application/json", show)
+	}
+
+	}
+
+	func GetAllBook(ctx *gin.Context){
+		ctx.JSON(http.StatusOK,bookDatas)
+	}
--- routers/bookRouter.go
+++ routers/bookRouter.go
@@ -9,8 +9,11 @@ import (
 
 func StartServer() *gin.Engine {
 	router := gin.Default()
+	router.GET("/books", controllers.GetAllBook)
 	router.POST("/books", controllers.CreateBook)
-
+	router.GET("/books/:id", controllers.GetBook)
+	router.PUT("/books/:id", controllers.UpdateBook)
+	router.DELETE("/books/:id", controllers.DeleteBook)
 
 	
 	return router
