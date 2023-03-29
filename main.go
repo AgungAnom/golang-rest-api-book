3db63c3abd3e36bda6f133be0c210e7cafd0577a
I Gusti Agung Anom Semara Putra <agoenkanom1305@gmail.com>
2023-03-29 10:39:03 +0800

    fix json name

--- main.go
+++ main.go
@@ -1,12 +1,10 @@
 package main
 
 import (
-	"golang-rest-api-book/database"
 	"golang-rest-api-book/routers"
 )
 
 const PORT = ":4000"
 func main(){
-	database.RunDB()
 	routers.StartServer().Run(PORT)
 }
--- models/book.go
+++ models/book.go
@@ -8,6 +8,6 @@ type Book struct{
 	ID uint ` gorm:"primaryKey" json:"id"`
 	Title string `gorm:"not null; type:varchar(255)" json:"name_book"`
 	Author string `gorm:"not null; type:varchar(255)" json:"author"`
-	CreatedAt time.Time
-	UpdatedAt time.Time
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
 }
